adb98b748af6cf0300c64625c2d620758376a771
MarjonaSodiqova <sodiqovamarjona0@gmail.com>
2025-02-24 11:13:36 +0500

    4

--- lesson-5/homework/temperature.py
+++ lesson-5/homework/temperature.py
@@ -5,7 +5,7 @@ def convert_far_to_cel(f):
     return (f - 32) * 5 / 9
 
 f_temp = float(input("Enter a temperature in degrees F: "))
-print(f"{f_temp} degrees F = {convert_far_to_cel(f_temp):.2f} degrees C")
+print(f"{f_temp} degrees F = {convert_far_to_cel(f_temp):.2f} degrees in C")
 
 c_temp = float(input("Enter a temperature in degrees C: "))
-print(f"{c_temp} degrees C = {convert_cel_to_far(c_temp):.2f} degrees F")
+print(f"{c_temp} degrees C = {convert_cel_to_far(c_temp):.2f} degrees in F")
